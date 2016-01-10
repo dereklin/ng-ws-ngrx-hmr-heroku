19efc83fafd693c7bd0fc2c7cac0f37295b360ed
Derek Lin <Derek Lin>
2016-01-10 14:45:29 -0800

    test

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('danshui', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
