63e8780243485455f67a41abd81f60959179331d
Derek Lin <Derek Lin>
2017-03-08 21:50:15 -0800

    highlight matching

--- src/app/navigation/index.ts
+++ src/app/navigation/index.ts
@@ -1,3 +1,5 @@
+import { NgbHighlightComponent } from './ngb-highlight.component';
+import { FormsModule } from '@angular/forms';
 import { BootstrapTabNavComponent } from './bootstrap-tab-nav/bootstrap-tab-nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -6,14 +8,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { SidebarHandleComponent } from './sidebar-handle/sidebar-handle.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [BrowserModule, RouterModule],
+  imports: [BrowserModule,
+    RouterModule,
+    NgbModule.forRoot(),
+    FormsModule],
   declarations: [TopbarComponent, SidebarComponent,
     FooterComponent, BootstrapTabNavComponent,
-    SidebarHandleComponent, FooterComponent],
+    SidebarHandleComponent, FooterComponent,
+    NgbHighlightComponent],
   exports: [TopbarComponent, SidebarComponent,
     FooterComponent, BootstrapTabNavComponent,
-    SidebarHandleComponent, FooterComponent]
+    SidebarHandleComponent, FooterComponent,
+    NgbHighlightComponent]
 })
 export class NavigationModule { }
---
+++ src/app/navigation/ngb-highlight.component.ts
@@ -0,0 +1,48 @@
+import {Component, Input, OnChanges, ChangeDetectionStrategy, SimpleChanges} from '@angular/core';
+
+export function regExpEscape(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+
+export function toString(value: any): string {
+  return (value !== undefined && value !== null) ? `${value}` : '';
+}
+
+@Component({
+  selector: 'ngb-highlight',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `<template ngFor [ngForOf]="parts" let-part let-isOdd="odd">` +
+      `<span *ngIf="isOdd" >{{part}}</span>` +
+      `<template [ngIf]="!isOdd"><span class="{{highlightClass}}">{{part}}</span></template>` +
+      `</template>`,
+      // template needs to be formatted in a certain way so we don't add empty text nodes
+  styles: [`
+    .ngb-highlight {
+      font-weight: bold;
+    }
+  `]
+})
+export class NgbHighlightComponent implements OnChanges {
+  public parts: string[];
+
+  @Input() public highlightClass = 'ngb-highlight';
+  @Input() public result: string;
+  @Input() public term: string;
+
+  public ngOnChanges(changes: SimpleChanges) {
+    const resultStr = toString(this.result);
+    const resultLC = resultStr.toLowerCase();
+    const termLC = toString(this.term).toLowerCase();
+    let currentIdx = 0;
+
+    if (termLC.length > 0) {
+      this.parts = resultLC.split(new RegExp(`(${regExpEscape(termLC)})`)).map((part) => {
+        const originalPart = resultStr.substr(currentIdx, part.length);
+        currentIdx += part.length;
+        return originalPart;
+      });
+    } else {
+      this.parts = [resultStr];
+    }
+  }
+}
