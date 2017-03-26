748ef6f8499703bc2d56dc9f5de65ffe7787e62f
Derek Lin <Derek Lin>
2017-03-25 20:30:07 -0700

    clean up node server code

--- web.js
+++ web.js
@@ -1,18 +1,8 @@
 var express = require('express');
-var path = require('path');
 var app = express();
-// var cool = require('cool-ascii-faces');
-
-var routes = require('./routes/index');
-
-//view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/dist'));
-
-app.use('/', routes);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
