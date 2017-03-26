ecd0fc41d44ec58ddb808bfab2c564006fda8db0
Derek Lin <Derek Lin>
2017-03-25 23:11:44 -0700

    test

--- web.js
+++ web.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
-var routes = require('./routes/index');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/dist'));
-app.use('/', routes);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
